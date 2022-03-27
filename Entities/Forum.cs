5b4f229213d104ccc40cd139a233bd68ea2c4ded
RodrigoValente1906 <rodrigo.valente.2002@gmail.com>
2022-03-27 14:59:16 +0200

    some changes

--- Entities/Forum.cs
+++ Entities/Forum.cs
@@ -6,8 +6,7 @@ public class Forum
     public string ForumDescription { get; set; }
     public int Id { get; set; }
     public ICollection<SubForum> SubForums { get; set; }
-    public ICollection<User> Users { get; set; }
-    
+
     public Forum() 
     {
         SubForums = new List<SubForum>();
