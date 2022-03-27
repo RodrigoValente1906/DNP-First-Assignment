3a5b7b1881df7ff7c3d9a27d68b130f9c8904829
RodrigoValente1906 <rodrigo.valente.2002@gmail.com>
2022-03-27 15:02:20 +0200

    some changes

--- Entities/Post.cs
+++ Entities/Post.cs
@@ -3,13 +3,11 @@ namespace Entities;
 public class Post
 {
     public int Id { get; set; }
-    public string Owner { get; set; }
     public string Header { get; set; }
     public string Body { get; set; }
 
-    public Post(string owner, string header, string body)
+    public Post(string header, string body)
     {
-        Owner = owner;
         Header = header;
         Body = body;
     }
