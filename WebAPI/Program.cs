e387996965c61e6cc3021912d71c352737ba0da4
RodrigoValente1906 <rodrigo.valente.2002@gmail.com>
2022-04-25 09:50:27 +0200

    WebAPI

---
+++ WebAPI/Program.cs
@@ -0,0 +1,33 @@
+using Application;
+using JsonDataAccess;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IForumDAO, ForumDAOImpl>();
+builder.Services.AddScoped<IUserDAO, UserDAOImpl>();
+builder.Services.AddScoped<JsonForumContext>();
+builder.Services.AddScoped<JsonUserContext>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
